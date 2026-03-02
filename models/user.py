f05cf32603ea6e166f47623b32d92c79818a3378
Raouf Abdallah <raoufabdallah0909@gmail.com>
2026-03-02 18:24:30 +0200

    Fixed merge conflict in User model

--- models/user.py
+++ models/user.py
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 from sqlalchemy import Column , String , Integer , Float
 from sqlalchemy.orm import relationship
-=======
-from sqlalchemy import Column , String , Integer , Float ,CheckConstraint
->>>>>>> ecd9845a080602ad4b34a9f85fcb754409ad4d9a
 from db_main import Base
 
 
