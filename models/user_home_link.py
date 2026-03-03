491c05573a700344788590c58cf0a65636b53a43
hadi-hajj <hajjhousseinhadi631@gmail.com>
2026-03-03 09:31:53 +0200

    i fixed many to many rel

--- models/user_home_link.py
+++ models/user_home_link.py
@@ -3,8 +3,10 @@ from db_main import Base
 from user import User
 from sqlalchemy.orm import relationship
 from sqlalchemy import ForeinKey
+from home import Home 
+
 class Home(Base):
     __tablename__ = 'user_home_link'
     id = Column(Integer, primary_key=True)
-    home_id = Column(Integer, ForeinKey('home.id'), nullable=False)
-    user_id = Column(Integer, ForeinKey('user.id'), nullable=False)
+    home_id = Column(Integer, ForeinKey('Home.id'), nullable=False)
+    user_id = Column(Integer, ForeinKey('User.id'), nullable=False)
