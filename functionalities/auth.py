91a6c966c12933266a988ecb825662675ccd589f
hadi-hajj <hajjhousseinhadi631@gmail.com>
2026-03-06 14:40:29 +0200

    auth done ba3d fi js

--- functionalities/auth.py
+++ functionalities/auth.py
@@ -4,11 +4,12 @@ from fastapi import HTTPException, Depends
 from fastapi.security import OAuth2PasswordBearer
 NGU = "~`3781678^&*^^roro!&%#^%&^!*)(&%$^*N!)(*M7nhadi42868&!)&#^$)(*!(&Btrgnhm(^!(THK*^(@$&^% ?/>,.,<<<>,6M7*&m@ t!*&oIUGHXharde#tp@$sNJS))))"
 ALGORITHM = "HS256"
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
-def  create_acess_token(sub:str,expires_minutes:int = 15):
-    expire = datetime.now(timezone.utc) + timedelta(minutes = expires_minutes)
-    payload= {"sub":sub,"exp":expire}
-    return jwt.encode(payload,NGU,algorithm=ALGORITHM)
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
+def  create_access_token(data: dict, expires_minutes: int = 15):
+    payload = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=expires_minutes)
+    payload["exp"] = expire
+    return jwt.encode(payload, NGU, algorithm=ALGORITHM)
 
 def get_curr_user_id(token:str= Depends(oauth2_scheme)):
     payload = decode_token(token)
--- routers/login.py
+++ routers/login.py
@@ -5,9 +5,9 @@ from db_main import SessionLocal
 from models.user import User
 from models.company import Company
 router = APIRouter()
-hash = CryptContext(schemes=["bcrypt"], deprecated = "auto")
-
- 
+#hash = CryptContext(schemes=["bcrypt"], deprecated = "auto")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+from functionalities.auth import create_access_token
 
 def get_db():
     db = SessionLocal()
@@ -17,22 +17,36 @@ def get_db():
         db.close()
 
 @router.post("/login")
-def login(username: str = Form(...), password: str = Form(...), db: Session = Depends(get_db)):
-    # Check users table first
-    user = db.query(User).filter(User.email == username).first()
-    if user:
-        if not hash.verify(password, user.password):
-            raise HTTPException(status_code=400, detail="Invalid email or password")
-        return {"message": "Login successful", "user_id": user.id, "role": "user"}
+def login(email: str = Form(...), password: str = Form(...), db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.email == email).first()
+    if user and pwd_context.verify(password, user.password):
+        token = create_access_token(
+            data={"sub": str(user.id), "role": "user"},
+            expires_minutes=15
+        )
+        return {
+            "access_token": token,
+            "token_type": "bearer",
+            "role": "user",
+            "user_id": user.id
+        }
+    #auth toke  
 
     # Check companies table
-    company = db.query(Company).filter(Company.email == username).first()
-    if company:
-        if not hash.verify(password, company.password):
-            raise HTTPException(status_code=400, detail="Invalid email or password")
-        return {"message": "Login successful", "company_id": company.id, "role": "company"}
-
-    raise HTTPException(status_code=400, detail="Invalid email or password")
+    company = db.query(Company).filter(Company.email == email).first()
+    if company and pwd_context.verify(password, company.password):
+    
+        token = create_access_token(
+        data={"sub": str(company.id), "role": "company"},
+        expires_minutes=15
+        )
+        return {
+            "access_token": token,
+            "token_type": "bearer",
+            "role": "company",
+            "company_id": company.id
+        }
+    raise HTTPException(status_code=401, detail="Invalid email or password")
 
 
 
