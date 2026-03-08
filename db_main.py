ca586cd7f226fe283230cfdb5f052a266efce0d3
hadi-hajj <hajjhousseinhadi631@gmail.com>
2026-03-08 19:11:18 +0200

    nothing

--- db_main.py
+++ db_main.py
@@ -3,7 +3,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:123@localhost/Recycling"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 def get_db():
     db = SessionLocal()
